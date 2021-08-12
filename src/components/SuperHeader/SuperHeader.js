95fd0fe87620568123bbc633e1b7666d93fdaf2a
Sylvain DENYSE <sylvain.denyse@knplabs.com>
2021-08-12 17:56:54 +0200

    feat: superheader

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -26,10 +26,18 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+
+  display: flex;
+  align-items: center;
+  gap: 24px;
+  padding: 0 32px;
+  min-height: 40px;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+
+  margin-right: auto;
 `;
 
 const HelpLink = styled.a`
